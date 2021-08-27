162b48adbd7056411529b365de469aff7a4a8c88
Alexander Nguyen <41305327+nguyenalex214@users.noreply.github.com>
2021-08-26 17:42:23 -0700

    Practicing seeding data

---
+++ src/db/seeds/01-products.js
@@ -0,0 +1,10 @@
+const products = require("../fixtures/products");
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex 
+      .raw("TRUNCATE TABLE products RESTART IDENTITY CASCADE")
+      .then(function() {
+        return knex("products").insert(products);
+      });
+};
